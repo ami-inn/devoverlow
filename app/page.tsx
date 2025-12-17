17d0b33c119200e8c0d962af8b234d54190f884f
ami-inn <amipk2001@gmail.com>
2025-12-17 17:35:12 +0530

    update: the tailwind setup is done

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      
+      <h2 className='h2-bold text-light400_light500'>
+        Welcome to Next.js!
+      </h2>
     </div>
   )
 }
