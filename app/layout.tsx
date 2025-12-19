cc783817d16c33d09e0447a6a4de2cca5b5cad4f
ami-inn <amipk2001@gmail.com>
2025-12-19 11:11:51 +0530

    update folder and loyouts added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import localFont from "next/font/local";
 
 import "./globals.css";
 import ThemeProvider from "@/context/Theme";
-import Navbar from "@/components/navigation/navbar";
 
 const inter = localFont({
   src: "./fonts/interVF.ttf",
@@ -42,7 +41,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
