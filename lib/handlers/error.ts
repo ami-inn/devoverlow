a1c662990273d361c160e82bb9002c24ac5d3ec9
ami-inn <amipk2001@gmail.com>
2025-12-29 22:42:41 +0530

    configured the error handler with logger

--- lib/handlers/error.ts
+++ lib/handlers/error.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
 import { RequestError, ValidationError } from "../http-errors";
+import logger from "../logger";
 
 export type ResponseType = "api" | "server";
 
@@ -27,10 +28,10 @@ const formatResponse = (
 
 const handleError = (error: unknown, responseType: ResponseType = "server") => {
   if (error instanceof RequestError) {
-    // logger.error(
-    //   { err: error },
-    //   `${responseType.toUpperCase()} Error: ${error.message}`
-    // );
+    logger.error(
+      { err: error },
+      `${responseType.toUpperCase()} Error: ${error.message}`
+    );
 
     return formatResponse(
       responseType,
@@ -45,10 +46,10 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
       error.flatten().fieldErrors as Record<string, string[]> // flatten because ZodError fieldErrors can have undefined values and multiple types
     );
 
-    // logger.error(
-    //   { err: error },
-    //   `Validation Error: ${validationError.message}`
-    // );
+    logger.error(
+      { err: error },
+      `Validation Error: ${validationError.message}`
+    );
 
     return formatResponse(
       responseType,
@@ -59,11 +60,13 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
   }
 
   if (error instanceof Error) {
-    // logger.error(error.message);
+    logger.error(error.message);
 
     return formatResponse(responseType, 500, error.message);
   }
 
-//   logger.error({ err: error }, "An unexpected error occurred");
+  logger.error({ err: error }, "An unexpected error occurred");
   return formatResponse(responseType, 500, "An unexpected error occurred");
 };
+
+export default handleError;
