f4e16669050f9c938b51b31092aa3d118a6f18a0
ami-inn <amipk2001@gmail.com>
2025-12-29 20:24:05 +0530

    udpate collection model added

---
+++ database/collection.model.ts
@@ -0,0 +1,17 @@
+import { model, models, Schema } from "mongoose";
+
+
+export interface ICollection {
+    author : Schema.Types.ObjectId;
+    question : Schema.Types.ObjectId;
+
+}
+
+const collectionSchema = new Schema<ICollection>({
+    author : { type: Schema.Types.ObjectId, ref: "User", required: true },
+    question : { type: Schema.Types.ObjectId, ref: "Question", required: true },
+
+}, { timestamps: true })
+
+const Collection = models?.Collection || model<ICollection>("Collection", collectionSchema);    
+export default Collection;
