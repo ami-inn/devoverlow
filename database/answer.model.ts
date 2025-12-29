bc2ce21dac528f3370a8d5732697de19cc2b4805
ami-inn <amipk2001@gmail.com>
2025-12-29 20:07:53 +0530

    update: answer model added

---
+++ database/answer.model.ts
@@ -0,0 +1,24 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IAAnswerModel {
+  author: Types.ObjectId;
+  question: Types.ObjectId;
+  content: string;
+  upVotes?: number;
+  downVotes?: number;
+}
+
+const answerSchema = new Schema<IAAnswerModel>(
+  {
+    author: { type: Types.ObjectId, ref: "User", required: true },
+    question: { type: Types.ObjectId, ref: "Question", required: true }, // which question this answer belongs to // one question can have multiple answers but one answer belongs to one question
+    content: { type: String, required: true },
+    upVotes: { type: Number, default: 0 },
+    downVotes: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const Answer = models?.Answer || model<IAAnswerModel>("Answer", answerSchema);
+
+export default Answer;
