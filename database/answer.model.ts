b817cca94455048d19d69c83e4125c58d67776f9
ami-inn <amipk2001@gmail.com>
2025-12-29 20:19:44 +0530

    update: tag models created

--- database/answer.model.ts
+++ database/answer.model.ts
@@ -10,8 +10,8 @@ export interface IAAnswerModel {
 
 const answerSchema = new Schema<IAAnswerModel>(
   {
-    author: { type: Types.ObjectId, ref: "User", required: true },
-    question: { type: Types.ObjectId, ref: "Question", required: true }, // which question this answer belongs to // one question can have multiple answers but one answer belongs to one question
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    question: { type: Schema.Types.ObjectId, ref: "Question", required: true }, // which question this answer belongs to // one question can have multiple answers but one answer belongs to one question
     content: { type: String, required: true },
     upVotes: { type: Number, default: 0 },
     downVotes: { type: Number, default: 0 },
---
+++ database/tag.model.ts
@@ -0,0 +1,21 @@
+import { model, models, Schema } from "mongoose";
+
+export interface ITag {
+    name: string;
+    questions?: number;
+}
+
+const tagSchema = new Schema<ITag>({
+    name: { type: String, required: true,unique: true },
+    questions: { type: Number, default: 0 },
+}, { timestamps: true })
+
+const Tag = models?.Tag || model<ITag>("Tag", tagSchema);
+export default Tag;
+
+
+// two different tag model
+// tag have name and no of question
+// tagquestion have tag id and question id
+// many to many relationship between question and tag
+// one question can have multiple tags
