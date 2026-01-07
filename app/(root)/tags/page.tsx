e0195d406bce84cf7f43142198f91f0d1eefd331
ami-inn <amipk2001@gmail.com>
2026-01-07 23:19:18 +0530

    update: the tag display is completed

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,20 +1,54 @@
-import { getTags } from '@/lib/actions/tag.action'
-import React from 'react'
+import TagCard from "@/components/cards/TagCard";
+import DataRenderer from "@/components/DataRenderer";
+import LocalSearch from "@/components/search/LocalSearch";
 
-const page =async () => {
-  const{success,data,error} = await getTags({
-    page:1,
-    pageSize:10,
-    // query:'test',
-  })
+import ROUTES from "@/constants/routes";
+import { EMPTY_TAGS } from "@/constants/states";
+import { getTags } from "@/lib/actions/tag.action";
 
-  const {tags} = data || {}
-  console.log("tags in tag page:",JSON.stringify(tags,null,2));
-  return ( 
-    <div>
-      
-    </div>
-  )
-}
+const Tags = async ({ searchParams }: RouteParams) => {
+  const { page, pageSize, query, filter } = await searchParams;
 
-export default page
+  const { success, data, error } = await getTags({
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query,
+    filter,
+  });
+
+  const { tags, isNext } = data || {};
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900 text-3xl">Tags</h1>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch
+          route={ROUTES.TAGS}
+          imgSrc="/icons/search.svg"
+          placeholder="Search tags..."
+          otherClasses="flex-1"
+        />
+
+  
+      </div>
+
+      <DataRenderer
+        success={success}
+        error={error}
+        data={tags}
+        empty={EMPTY_TAGS}
+        render={(tags) => (
+          <div className="mt-10 flex w-full flex-wrap gap-4">
+            {tags.map((tag) => (
+              <TagCard key={tag._id} {...tag} />
+            ))}
+          </div>
+        )}
+      />
+
+         </>
+  );
+};
+
+export default Tags;
