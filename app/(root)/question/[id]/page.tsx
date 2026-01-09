16b4558219fac35a6f4f8386b3e75ecab7f5a13a
ami-inn <amipk2001@gmail.com>
2026-01-09 16:03:29 +0530

    update question form added

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,5 +1,6 @@
 import TagCard from "@/components/cards/TagCard";
 import { Preview } from "@/components/editor/Preview";
+import AnswerForm from "@/components/forms/AnswerForm";
 import Metric from "@/components/Metric";
 import UserAvatar from "@/components/UserAvatar";
 import ROUTES from "@/constants/routes";
@@ -193,11 +194,11 @@ const Question = async ({ params, searchParams }: RouteParams) => {
       </section>
 
       <section className="my-5">
-        {/* <AnswerForm
+        <AnswerForm
           questionId={question._id}
           questionTitle={question.title}
           questionContent={question.content}
-        /> */}
+        />
       </section>
     </>
   );
