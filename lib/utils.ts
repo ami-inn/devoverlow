b741f532c78385d433e0b574bd5748070b03f228
ami-inn <amipk2001@gmail.com>
2026-02-20 13:00:02 +0530

    implement all user actions

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,7 +7,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// we are using devicon for tech stack icons
 export function getDeviconClassName(techName: string) {
   const normalizedTech = techName.replace(/[ .]/g, "").toLowerCase();
 
@@ -168,33 +167,33 @@ export const getTimeStamp = (createdAt: Date): string => {
   return `${diffDays} days ago`;
 };
 
-// export function assignBadges(params: {
-//   criteria: {
-//     type: keyof typeof BADGE_CRITERIA;
-//     count: number;
-//   }[];
-// }) {
-//   const badgeCounts: Badges = {
-//     GOLD: 0,
-//     SILVER: 0,
-//     BRONZE: 0,
-//   };
+export function assignBadges(params: {
+  criteria: {
+    type: keyof typeof BADGE_CRITERIA;
+    count: number;
+  }[];
+}) {
+  const badgeCounts: Badges = {
+    GOLD: 0,
+    SILVER: 0,
+    BRONZE: 0,
+  };
 
-//   const { criteria } = params;
+  const { criteria } = params;
 
-//   criteria.forEach((item) => {
-//     const { type, count } = item;
-//     const badgeLevels = BADGE_CRITERIA[type];
+  criteria.forEach((item) => {
+    const { type, count } = item;
+    const badgeLevels = BADGE_CRITERIA[type];
 
-//     Object.keys(badgeLevels).forEach((level) => {
-//       if (count >= badgeLevels[level as keyof typeof badgeLevels]) {
-//         badgeCounts[level as keyof Badges] += 1;
-//       }
-//     });
-//   });
+    Object.keys(badgeLevels).forEach((level) => {
+      if (count >= badgeLevels[level as keyof typeof badgeLevels]) {
+        badgeCounts[level as keyof Badges] += 1;
+      }
+    });
+  });
 
-//   return badgeCounts;
-// }
+  return badgeCounts;
+}
 
 export function processJobTitle(title: string | undefined | null): string {
   // Check if title is undefined or null
